5b77e348f76c8b0a42d058381454dafb2cecbdf7
Aarush Verma <aarushverma@Aarushs-MacBook-Pro.local>
2022-08-29 17:20:00 -0500

    initial

---
+++ Step 1.1/Ifelse.java
@@ -0,0 +1,13 @@
+public class Ifelse {
+
+    static String compareNM(int n, int m) {
+        if (n < m) {
+            return "lesser";
+        } else if (n > m) {
+            return "greater";
+        } else {
+            return "equal";
+        }
+
+    }
+}
---
+++ Step 1.1/WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop {
+
+    static void printTable(int n) {
+        int multiplier = 10;
+        while (multiplier > 0) {
+            System.out.print(n * multiplier + " ");
+            multiplier--;
+        }
+
+    }
+
+}
