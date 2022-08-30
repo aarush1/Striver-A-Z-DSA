8d5fe60dd5b5408cb60c55a1351c0727f097a19f
Aarush Verma <aarushverma@Aarushs-MBP.dhost.uta.edu>
2022-08-29 20:32:19 -0500

    pattern till 5

---
+++ Step 1/Step 1.2/Pattern1.java
@@ -0,0 +1,13 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+
+        for (int j = 0; j < 5;  j++){
+
+            for (int i = 0; i < 5; i++) {
+                System.out.print("*");
+                //System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
